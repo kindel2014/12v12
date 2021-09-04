480e630ac11e10676b7c4f2083b6b305432ae42a
Lovro Magaš <63733825+lovro-m@users.noreply.github.com>
2021-09-04 11:34:37 +0200

    Linken Shield Rewrite (#345)
    * Linken Shield Rewrite
    
    * PR review
    
    * Localization

--- game/scripts/vscripts/common/game_perks/modifier_lib/linken.lua
+++ game/scripts/vscripts/common/game_perks/modifier_lib/linken.lua
@@ -1,18 +1,110 @@
 require("common/game_perks/base_game_perk")
 
+LinkLuaModifier("linken_ready", "common/game_perks/modifier_lib/linken", LUA_MODIFIER_MOTION_NONE)
+LinkLuaModifier("linken_cooldown", "common/game_perks/modifier_lib/linken", LUA_MODIFIER_MOTION_NONE)
 linken = class(base_game_perk)
+
+function linken:IsHidden() return true end
+function linken:DeclareFunctions() return { MODIFIER_PROPERTY_ABSORB_SPELL } end
 function linken:GetTexture() return "perkIcons/linken" end
 
-linken.OnCreated = function(self)
-	if not IsServer() then return end
-	self:GetParent():AddItemByName("item_linken_perk_" .. self.v)
+function linken:GetAbsorbSpell(params)
+	if not IsServer() then return nil end
+	local parent = self:GetParent()
+	if not parent or parent:IsNull() then return nil end
+	
+	if params.ability:GetCaster():GetTeamNumber() == parent:GetTeamNumber() then return nil end
+	if parent:FindModifierByName("linken_cooldown") then return nil end
+
+	-- modifier_item_sphere_target already has highest priority, so no need to check that
+	local sphere_modifier = parent:FindModifierByName("modifier_item_sphere")
+	if sphere_modifier and not sphere_modifier:IsNull() then
+		local item = sphere_modifier:GetAbility()
+		if item and not item:IsNull() then
+			local is_cd_ready = item:IsCooldownReady()
+			if is_cd_ready then return nil end
+		end
+	end
+
+	local qop_modifier = parent:FindModifierByName("modifier_special_bonus_spell_block")
+	if qop_modifier and not qop_modifier:IsNull() then
+		local remaining_time = qop_modifier:GetRemainingTime()
+		if remaining_time and remaining_time <= 0 then return nil end
+	end
+
+	parent:EmitSound("DOTA_Item.LinkensSphere.Activate")
+	
+	local pfx = ParticleManager:CreateParticle("particles/items_fx/immunity_sphere.vpcf", PATTACH_POINT_FOLLOW, parent)
+	ParticleManager:SetParticleControlEnt(pfx, 0, parent, PATTACH_POINT_FOLLOW, "attach_hitloc", parent:GetAbsOrigin(), true)
+	ParticleManager:ReleaseParticleIndex(pfx)
+	
+	parent:AddNewModifier(parent, self:GetAbility(), "linken_cooldown", {duration = self.v})
+	local modifier = parent:FindModifierByName("linken_ready")
+	if modifier and not modifier:IsNull() then
+		parent:RemoveModifierByName("linken_ready")
+	end
+	
+	return 1
 end
 
+linken_cooldown = class({})
+
+function linken_cooldown:IsHidden() return false end
+function linken_cooldown:IsDebuff() return false end
+function linken_cooldown:IsPurgable() return false end
+function linken_cooldown:GetTexture() return "perkIcons/linken" end
+function linken_cooldown:GetAttributes() return MODIFIER_ATTRIBUTE_IGNORE_INVULNERABLE end
+
+function linken_cooldown:OnDestroy()
+	if IsServer() then
+		self:GetParent():AddNewModifier(self:GetParent(), self:GetAbility(), "linken_ready", {})
+	end
+end
+
+linken_ready = class({})
+
+function linken_ready:IsHidden() return false end
+function linken_ready:IsDebuff() return false end
+function linken_ready:IsPurgable() return false end
+function linken_ready:GetTexture() return "perkIcons/linken" end
+function linken_ready:GetAttributes() return MODIFIER_ATTRIBUTE_IGNORE_INVULNERABLE end
+
+function linken_ready:GetEffectName()
+	return "particles/linken_orb_small.vpcf"
+end
+
+function linken_ready:GetEffectAttachType()
+	return PATTACH_OVERHEAD_FOLLOW
+end
+
+function linken_ready:ShouldUseOverheadOffset()
+	return true
+end
+
+
+
 linken_t0 = class(linken)
-linken_t0.v = 0
 linken_t1 = class(linken)
-linken_t1.v = 1
 linken_t2 = class(linken)
-linken_t2.v = 2
 linken_t3 = class(linken)
-linken_t3.v = 3
+
+function linken_t0:OnCreated() 
+	if not IsServer() then return end 
+	self.v = 90
+	self:GetParent():AddNewModifier(self:GetParent(), self:GetAbility(), "linken_ready", {})
+end
+function linken_t1:OnCreated() 
+	if not IsServer() then return end 
+	self.v = 60 
+	self:GetParent():AddNewModifier(self:GetParent(), self:GetAbility(), "linken_ready", {})
+end
+function linken_t2:OnCreated() 
+	if not IsServer() then return end 
+	self.v = 30 
+	self:GetParent():AddNewModifier(self:GetParent(), self:GetAbility(), "linken_ready", {})
+end
+function linken_t3:OnCreated() 
+	if not IsServer() then return end 
+	self.v = 15 
+	self:GetParent():AddNewModifier(self:GetParent(), self:GetAbility(), "linken_ready", {})
+end
