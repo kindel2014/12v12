34e3cf669e7b4f48695efda347b0687daad0fc5b
Sheodar <kkabachok@gmail.com>
2021-09-06 17:53:15 +0300

    Fixed %hp perk, RU text for linken shield perk

--- game/scripts/vscripts/common/game_perks/modifier_lib/bonus_hp_pct.lua
+++ game/scripts/vscripts/common/game_perks/modifier_lib/bonus_hp_pct.lua
@@ -2,16 +2,40 @@ require("common/game_perks/base_game_perk")
 
 bonus_hp_pct = class(base_game_perk)
 
-function bonus_hp_pct:DeclareFunctions() return { MODIFIER_PROPERTY_EXTRA_HEALTH_PERCENTAGE } end
+function bonus_hp_pct:DeclareFunctions() return { MODIFIER_PROPERTY_HEALTH_BONUS  } end
 function bonus_hp_pct:GetTexture() return "perkIcons/bonus_hp_pct" end
-function bonus_hp_pct:GetModifierExtraHealthPercentage() return self.v end
+function bonus_hp_pct:GetModifierHealthBonus()	return self.bonus_hp end
+
+function bonus_hp_pct:CalculateBonusHP()
+	if not IsServer() then return end
+	local parent = self:GetParent()
+	local current_max_hp = parent:GetMaxHealth() - self.bonus_hp
+	
+	self.bonus_hp = current_max_hp * self.v
+	parent:CalculateStatBonus(true)
+end
+
+bonus_hp_pct.OnCreated = function(self)
+	if not IsServer() then return end
+
+	local parent = self:GetParent()
+
+	self.last_max_hp = parent:GetMaxHealth()
+	self.bonus_hp = 0
+
+	self:CalculateBonusHP()
+
+	Timers:CreateTimer(1, function()
+		self:CalculateBonusHP()
+		return 1
+	end)
+end
 
 bonus_hp_pct_t0 = class(bonus_hp_pct)
+bonus_hp_pct_t0.v = 0.1
 bonus_hp_pct_t1 = class(bonus_hp_pct)
+bonus_hp_pct_t1.v = 0.2
 bonus_hp_pct_t2 = class(bonus_hp_pct)
+bonus_hp_pct_t2.v = 0.4
 bonus_hp_pct_t3 = class(bonus_hp_pct)
-
-function bonus_hp_pct_t0:OnCreated() self.v = 10 end
-function bonus_hp_pct_t1:OnCreated() self.v = 20 end
-function bonus_hp_pct_t2:OnCreated() self.v = 40 end
-function bonus_hp_pct_t3:OnCreated() self.v = 80 end
+bonus_hp_pct_t3.v = 0.8
