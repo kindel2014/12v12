a51af448e07fca1a96127e3f82f15971a9ec8ad4
ZLOY5 <ZLOY5@ukr.net>
2022-02-24 21:23:25 +0200

    Update protected_custom_events.js

--- content/panorama/layout/custom_game/common/protected_custom_events.js
+++ content/panorama/layout/custom_game/common/protected_custom_events.js
@@ -15,19 +15,7 @@ GameEvents.SendCustomGameEventToServer("secret_token", { token: token });
 $.Msg("Server identity token: "+token)
 
 GameEvents.SubscribeProtected = function(event_name, callback) {
-	return GameEvents.Subscribe(event_name, (event) => {
-		const player_id = Game.GetLocalPlayerID() 
-		// Spectators cant send token to server and have to ignore this check
-		// This give possibility to ruin spectators custom UI to cheaters, but I think it's not very important
-		// Maybe can be fixed if we will generate tokens on server side and send it to clients
-		if (player_id == -1 || token == event.chc_secret_token) {
-			callback(event.event_data);
-		} else {
-			$.Msg(
-				`Registered event ${event_name} have wrong server token: ${event.chc_secret_token}, for CUSTOM clientside events use GameEvents.SendEventClientSideProtected`,
-			);
-		}
-	});
+	return GameEvents.Subscribe(event_name, callback);
 };
 
 GameEvents.SendEventClientSideProtected = function(event_name, event_data) {
@@ -35,5 +23,5 @@ GameEvents.SendEventClientSideProtected = function(event_name, event_data) {
 		event_data: event_data,
 		chc_secret_token: token,
 	}
-	GameEvents.SendEventClientSide(event_name, event)
+	GameEvents.SendEventClientSide(event_name, event_data)
 }
